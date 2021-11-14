aa56a81d8aa581e45e29b5bfa0f326b86f432d0e
jelichka <katherine.jelich@mail.utoronto.ca>
2021-11-14 18:42:41 -0500

    Implement GetMatchesActivity, GetMatchesViewModel, GetMatchesPresenter and related classes.

---
+++ app/src/main/java/com/example/cupetfrontend/GetMatchesResult.java
@@ -0,0 +1,41 @@
+package com.example.cupetfrontend;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+/**
+ * Registration result : success (user details) or error message.
+ */
+
+public class GetMatchesResult {
+    private final boolean isError;
+    private final String errorMessage;
+    @Nullable
+    private List<String> matches;
+
+    public GetMatchesResult(boolean isError) {
+        this.isError = isError;
+        this.errorMessage = "error getting Pet Matches";
+        matches = null;
+    }
+
+    public GetMatchesResult(boolean isError, @Nullable List<String> matches) {
+        this.isError = isError;
+        errorMessage = "";
+        this.matches = matches;
+    }
+
+    public boolean isError() {
+        return isError;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    @Nullable
+    public List<String> getMatches() {
+        return matches;
+    }
+}
