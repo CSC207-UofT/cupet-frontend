e7eb3846a797128e0e07b95ee61c078833054eac
jelichka <katherine.jelich@mail.utoronto.ca>
2021-11-28 03:21:21 -0500

    Update ViewPetsActivity and related classes

--- app/src/main/java/com/example/cupetfrontend/ViewPetsActivity.java
+++ app/src/main/java/com/example/cupetfrontend/ViewPetsActivity.java
@@ -2,11 +2,13 @@ package com.example.cupetfrontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,11 +20,17 @@ public class ViewPetsActivity extends AppCompatActivity {
     private final ArrayList<String> mPetNames = new ArrayList<>();
     private final ArrayList<String> mPetImageUrls = new ArrayList<>();
 
+    private Button nAddPetButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pets);
         Log.d(TAG, "onCreate: started.");
+
+        nAddPetButton = findViewById(R.id.add_pet_button);
+        setUpAddPetButtonListener();
 
         initImageBitmaps(); // gets the bitmaps
     }
@@ -42,22 +50,22 @@ public class ViewPetsActivity extends AppCompatActivity {
         mPetImageUrls.add("https://live.staticflickr.com/3373/3600836516_ab924c6729_b.jpg");
         mPetNames.add("Dogster");
 
-//
-//        mPetImageUrls.add("https://static.im-a-puzzle.com/gallery/Animals/Cats/Black-cat-watching.jpg");
-//        mPetNames.add("Caterson");
-//
-//        mPetImageUrls.add("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-photos-1593441022.jpg?crop=0.669xw:1.00xh;0.166xw,0&resize=640:*");
-//        mPetNames.add("Kitten");
-//
-//
-//        mPetImageUrls.add("https://cms.bbcearth.com/sites/default/files/2021-01/rein1.jpg");
-//        mPetNames.add("Reindeer");
-//
-//        mPetImageUrls.add("https://i.pinimg.com/originals/c9/b4/65/c9b46554f9e672374a93dc81ebd799ab.jpg");
-//        mPetNames.add("Albert");
-//
-//        mPetImageUrls.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIDYmgpoRu2NgmDdD34Wzl5FP6fbUOsFK0ixcHVIwPRMnez8wIPDphMdxHGCMza4WIB8s&usqp=CAU");
-//        mPetNames.add("Scoob");
+
+        mPetImageUrls.add("https://static.im-a-puzzle.com/gallery/Animals/Cats/Black-cat-watching.jpg");
+        mPetNames.add("Caterson");
+
+        mPetImageUrls.add("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-photos-1593441022.jpg?crop=0.669xw:1.00xh;0.166xw,0&resize=640:*");
+        mPetNames.add("Kitten");
+
+
+        mPetImageUrls.add("https://cms.bbcearth.com/sites/default/files/2021-01/rein1.jpg");
+        mPetNames.add("Reindeer");
+
+        mPetImageUrls.add("https://i.pinimg.com/originals/c9/b4/65/c9b46554f9e672374a93dc81ebd799ab.jpg");
+        mPetNames.add("Albert");
+
+        mPetImageUrls.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIDYmgpoRu2NgmDdD34Wzl5FP6fbUOsFK0ixcHVIwPRMnez8wIPDphMdxHGCMza4WIB8s&usqp=CAU");
+        mPetNames.add("Scoob");
 
         initRecyclerView(); // once it has img urls, it initializes the recycler view
     }
@@ -69,6 +77,16 @@ public class ViewPetsActivity extends AppCompatActivity {
         CardRecyclerViewAdapter adapter = new CardRecyclerViewAdapter(this, mPetNames, mPetImageUrls);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager((new GridLayoutManager(this, 2)));
+    }
+
+
+    private void setUpAddPetButtonListener(){
+        nAddPetButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Add Pet", Toast.LENGTH_SHORT).show();
+                //TODO: Link to Add New Pet Activity
+            }
+        });
     }
 
 
