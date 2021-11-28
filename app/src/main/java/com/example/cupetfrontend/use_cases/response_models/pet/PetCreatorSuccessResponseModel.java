287b66b27f2c267786415a95faebf84746f0fb1e
Andrew Qiu <andrewqiu9774@gmail.com>
2021-11-28 17:10:57 -0500

    Refactor to remove duplicate code in missing request/response models

--- app/src/main/java/com/example/cupetfrontend/use_cases/response_models/pet/PetCreatorSuccessResponseModel.java
+++ app/src/main/java/com/example/cupetfrontend/use_cases/response_models/pet/PetCreatorSuccessResponseModel.java
@@ -1,37 +1,10 @@
 package com.example.cupetfrontend.use_cases.response_models.pet;
 
-public class PetCreatorSuccessResponseModel {
-    private final String name;
-    private final String age;
-    private final String breed;
-    private final String biography;
-    private final String petId;
+import com.example.cupetfrontend.use_cases.data_models.PetData;
 
-    public PetCreatorSuccessResponseModel(String name, String age, String breed, String biography, String petId) {
-        this.name = name;
-        this.age = age;
-        this.breed = breed;
-        this.biography = biography;
-        this.petId = petId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public String getBreed() {
-        return breed;
-    }
-
-    public String getBiography() {
-        return biography;
-    }
-
-    public String getPetId() {
-        return petId;
+public class PetCreatorSuccessResponseModel extends PetData {
+    public PetCreatorSuccessResponseModel(String name, String age,
+                                          String breed, String biography, String petId) {
+        super(name, age, breed, biography, petId);
     }
 }
