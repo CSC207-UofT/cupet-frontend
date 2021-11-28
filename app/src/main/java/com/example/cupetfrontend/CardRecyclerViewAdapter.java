660ec06bead08f079138db9e50d5b2cf8cff082e
jelichka <katherine.jelich@mail.utoronto.ca>
2021-11-27 19:42:06 -0500

    Implement new View all Pets Page

---
+++ app/src/main/java/com/example/cupetfrontend/CardRecyclerViewAdapter.java
@@ -0,0 +1,96 @@
+package com.example.cupetfrontend;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class CardRecyclerViewAdapter extends RecyclerView.Adapter<CardRecyclerViewAdapter.ViewHolder>{
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private final ArrayList<String> mPetNames;
+    private final ArrayList<String> mPetImages;
+    private final Context mContext;
+
+    public CardRecyclerViewAdapter(Context mContext, ArrayList<String> mPetNames, ArrayList<String> mPetImages) {
+        this.mContext = mContext;
+        this.mPetNames = mPetNames;
+        this.mPetImages = mPetImages;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // This method is responsible for inflating the view
+        // Basically recycles the view holders - puts them in position they should be put into
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_carditem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        // important method - will change based on our layouts and what they should look like
+        Log.d(TAG, "onBindViewHolder: called."); // debugging
+
+        Glide.with(mContext)
+                // tells Glide we it as bitmap
+                .asBitmap()
+                // where we would reference img URLs - resource where img is coming from
+                .load(mPetImages.get(holder.getAdapterPosition()))
+                // loading image into image view - so reference view holder -> image widget
+                .into(holder.petImage);
+        holder.petName.setText(mPetNames.get(holder.getAdapterPosition()));
+
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on:" + mPetNames.get(holder.getAdapterPosition()));
+
+                Toast.makeText(mContext, mPetNames.get(holder.getAdapterPosition()), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    // tells adapter how many list items are in the list
+    @Override
+    public int getItemCount() {
+        return mPetNames.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView petImage;
+        TextView petName;
+
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            petImage = itemView.findViewById(R.id.card_pet_image);
+            petName = itemView.findViewById(R.id.card_pet_name);
+
+            parentLayout = itemView.findViewById(R.id.view_pets_card_layout);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cupetfrontend/ViewPetsActivity.java
@@ -0,0 +1,73 @@
+package com.example.cupetfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class ViewPetsActivity extends AppCompatActivity {
+
+    private static final String TAG = "NewGetMatchesActivity";
+
+    //vars
+    private final ArrayList<String> mPetNames = new ArrayList<>();
+    private final ArrayList<String> mPetImageUrls = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pets);
+        Log.d(TAG, "onCreate: started.");
+    }
+
+    private void initImageBitmaps(){
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+
+        mPetImageUrls.add("https://upload.wikimedia.org/wikipedia/commons/1/18/Dog_Breeds.jpg");
+        mPetNames.add("Doggo A");
+
+        mPetImageUrls.add("https://cdn.pixabay.com/photo/2020/11/12/15/43/japanese-spitz-5735829_1280.jpg");
+        mPetNames.add("Doggo B");
+
+        mPetImageUrls.add("https://assets.jamcity.com/20210419054719/snoopy-pop-illustration-icon-bottom-right.png");
+        mPetNames.add("Snoopy");
+
+        mPetImageUrls.add("https://live.staticflickr.com/3373/3600836516_ab924c6729_b.jpg");
+        mPetNames.add("Dogster");
+
+//
+//        mPetImageUrls.add("https://static.im-a-puzzle.com/gallery/Animals/Cats/Black-cat-watching.jpg");
+//        mPetNames.add("Caterson");
+//
+//        mPetImageUrls.add("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-photos-1593441022.jpg?crop=0.669xw:1.00xh;0.166xw,0&resize=640:*");
+//        mPetNames.add("Kitten");
+//
+//
+//        mPetImageUrls.add("https://cms.bbcearth.com/sites/default/files/2021-01/rein1.jpg");
+//        mPetNames.add("Reindeer");
+//
+//        mPetImageUrls.add("https://i.pinimg.com/originals/c9/b4/65/c9b46554f9e672374a93dc81ebd799ab.jpg");
+//        mPetNames.add("Albert");
+//
+//        mPetImageUrls.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIDYmgpoRu2NgmDdD34Wzl5FP6fbUOsFK0ixcHVIwPRMnez8wIPDphMdxHGCMza4WIB8s&usqp=CAU");
+//        mPetNames.add("Scoob");
+
+        initRecyclerView(); // once it has img urls, it initializes the recycler view
+    }
+
+    // method for actually setting up our recycler view
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: init recyclerview");
+        RecyclerView recyclerView = findViewById(R.id.card_recylcer_view);
+        CardRecyclerViewAdapter adapter = new CardRecyclerViewAdapter(this, mPetNames, mPetImageUrls);
+        //recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager((new GridLayoutManager(this, 2)));
+    }
+
+
+}
