8b85bacb073d0d50d8e3f40e22dfd84b1d45d1b5
Andrew Qiu <andrewqiu9774@gmail.com>
2021-12-04 16:08:17 -0500

    Add optional edit button to MainActivity

---
+++ app/src/main/java/com/example/cupetfrontend/ui/OnClickListener.java
@@ -0,0 +1,12 @@
+package com.example.cupetfrontend.ui;
+
+/**
+ * An interface defining a class responsible for listening to
+ * when some view is clicked.
+ */
+public interface OnClickListener {
+    /**
+     * A method that's called when the view is clicked
+     */
+    void onClick();
+}
