e2a87483532a4c6f14f10bc0578212a972526523
jelichka <katherine.jelich@mail.utoronto.ca>
2021-12-06 05:34:27 -0500

    Update PublicPetProfileFragment

--- app/src/main/java/com/example/cupetfrontend/ui/public_pet_profile/PublicPetProfileFragment.java
+++ app/src/main/java/com/example/cupetfrontend/ui/public_pet_profile/PublicPetProfileFragment.java
@@ -2,12 +2,16 @@ package com.example.cupetfrontend.ui.public_pet_profile;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.cupetfrontend.R;
 import com.example.cupetfrontend.controllers.abstracts.IPetController;
 import com.example.cupetfrontend.controllers.abstracts.IPetSessionManager;
@@ -16,6 +20,8 @@ import com.example.cupetfrontend.controllers.abstracts.IUserController;
 import com.example.cupetfrontend.databinding.FragmentPublicPetProfileBinding;
 import com.example.cupetfrontend.presenters.abstracts.IFetchPetProfilePresenter;
 import com.example.cupetfrontend.ui.MainActivityFragment;
+import com.example.cupetfrontend.ui.my_pet_profile.PetProfileResult;
+import com.example.cupetfrontend.ui.public_pet_profile.display_pet_profile_fragment.DisplayPetProfileViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -51,13 +57,78 @@ public class PublicPetProfileFragment extends MainActivityFragment {
         fetchPetProfilePresenter.setPetProfileViewModel(publicPetProfileViewModel);
         // fetchUserProfilePresenter.setUserProfileViewModel(publicPetProfileViewModel);
 
+        // get token from session manager
         ISessionManager sessionManager = dependencySelector.getSessionManager();
         String token = sessionManager.getToken();
+
+        String selectedPetId = "Selected PetId"; //TODO: Replace with obtained value
+        String selectedPetUserId = "Selected Pet's User ID"; //TODO: Replace with obtained value
+
+        publicPetProfileViewModel.fetchPetProfile(token, selectedPetId);
+
+
+        setUpObservePetProfileResult();
+        //setUpObserveUserProfileResult();
+        //
+
+
 
         return root;
 
     }
 
+    /**
+     * Set up this activity as an observer that observes the result of pet profile.
+     *
+     * Update the displayed views when the pet profile result has changed.
+     */
+    private void setUpObservePetProfileResult() {
+
+        publicPetProfileViewModel.fetchPetProfileResult().observe(getViewLifecycleOwner(), new Observer<PetProfileResult>() {
+            @Override
+            public void onChanged(@Nullable PetProfileResult petProfileResult) {
+                if (petProfileResult == null) {
+                    return;
+                }
+
+                if (petProfileResult.isError()){
+                    onPetProfileFailure(petProfileResult.getErrorMessage());
+                } else {
+                    onPetProfileSuccess(
+                            petProfileResult.getPetImage(),
+                            petProfileResult.getPetName(),
+                            petProfileResult.getPetAge(),
+                            petProfileResult.getPetBreed(),
+                            petProfileResult.getPetBio());
+                }
+            }
+        });
+    }
+
+    /**
+     * When success fetching a pet profile, set them to corresponding ImageView & TextViews.
+     */
+    private void onPetProfileSuccess(String petImageStr, String petNameStr, String petAgeStr, String petBreedStr, String petBioStr) {
+        if (petImageStr == "") {
+            petImageStr = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwn2UIl7YtjhkbxoIuoI4E7yyXH1KC6GvRqg&usqp=CAU";
+        }
+        // now fetch user profile?
+    }
+
+    /**
+     * Display a Fetch pet profile failed toast message.
+     * @param errorMessage The error message to display
+     */
+    private void onPetProfileFailure(String errorMessage) {
+        System.out.println("Fetch pet profile failed");
+        Toast.makeText(getApplicationContext(), "Fetch pet profile failed: " + errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Auto generated //
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
