55e9158779240971837ab1aa4829d37d693af6b1
jelichka <katherine.jelich@mail.utoronto.ca>
2021-12-08 04:50:15 -0500

    Update Javadoc in MatchedPetProfileFragment

--- app/src/main/java/com/example/cupetfrontend/ui/matched_pet_profile/MatchedPetProfileFragment.java
+++ app/src/main/java/com/example/cupetfrontend/ui/matched_pet_profile/MatchedPetProfileFragment.java
@@ -125,8 +125,4 @@ public class MatchedPetProfileFragment extends MainActivityFragment {
         Toast.makeText(getApplicationContext(), "Request failed" + errorMessage,
                 Toast.LENGTH_SHORT).show();
     }
-
-
-
-
 }
